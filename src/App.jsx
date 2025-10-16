76168e53c01d6ad393c18ba532b4c82db9cbb966
HowtoWebDev <developedbyEmre@gmail.com>
2025-10-16 13:45:49 +0300

    starter code

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <div className="relative min-h-screen flex justify-center items-center bg-[#0a0a0a] text-white p-4 md:py-14 font-mono overflow-hidden">
+        <h1 className="text-white text-8xl font-bold">STARTER CODE</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
